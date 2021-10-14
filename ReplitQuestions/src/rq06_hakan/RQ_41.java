8323744c630df4631970c9f4ffdc391756a5442f
Hakan Inal <89166736+Inal2021@users.noreply.github.com>
2021-10-14 22:39:35 +0200

    Update RQ_41
    14.10.2021

--- ReplitQuestions/src/rq06_hakan/RQ_41.java
+++ ReplitQuestions/src/rq06_hakan/RQ_41.java
@@ -1,4 +1,31 @@
 package rq06_hakan;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class RQ_41 {
+    /*Girilen metindeki tüm kelimelerin ilk harflerini büyük yapınız.(diğer harflere dokunmayın, büyükse büyük, küçükse küçük kalsın).
+
+    Örnek
+    makeTitle("Bu bir başlıktır")
+    Bu Bir Başlıktır
+
+    makeTitle("tüm ilk harfler büyük olacak")
+    Tüm İlk Harfler Büyük Olacak
+    * */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("please enter a wort or sentence: ");
+        String wort=scan.nextLine();
+
+        /*char harf=wort.charAt(0);
+
+        for (int i = 0; i < wort.length(); i++) {
+        if(wort.contains(" "))
+            wort.replaceFirst(harf,wort.toUpperCase(Locale.ROOT));
+        }*/
+
+
+
+    }
 }

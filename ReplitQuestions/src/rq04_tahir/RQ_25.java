5befe5c24f2e9269e30354bd289d838586e7520a
Tahir Cesur <89197324+TahirCesur@users.noreply.github.com>
2021-10-25 10:52:53 +0300

    Update RQ_25.java

--- ReplitQuestions/src/rq04_tahir/RQ_25.java
+++ ReplitQuestions/src/rq04_tahir/RQ_25.java
@@ -1,4 +1,24 @@
 package rq04_tahir;
 
 public class RQ_25 {
+  public static void main(String[] args) {
+		
+	    int rows = 5;
+
+	    for(int i = rows; i > 0; --i) {
+	    	
+	      for(int space = 0; space < rows - i; space++) {
+	        System.out.print("  ");
+	      }
+
+	      for(int j=0; j < 2 * i - 1; j++) {
+	        System.out.print("* ");
+	      }
+
+	      System.out.println();
+	    }
+	    
+	    System.out.println("Merhaba");
+	  
+	  }
 }

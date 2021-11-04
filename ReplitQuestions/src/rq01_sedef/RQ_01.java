ab9df9cece9b9f4eb2cfdeff8d6557807391d834
Hakan Inal <89166736+Inal2021@users.noreply.github.com>
2021-11-04 13:33:16 +0100

    v2

--- ReplitQuestions/src/rq01_sedef/RQ_01.java
+++ ReplitQuestions/src/rq01_sedef/RQ_01.java
@@ -1,4 +1,30 @@
 package rq01_sedef;
 
 public class RQ_01 {
+        /*1---
+    Sayinin Sayi degerlerinin toplamasini yapan Java kodu yaziniz.
+
+    Test Data
+    34
+    Beklenen Cikti
+    7
+    */
+
+    public static void main(String[] args) {
+
+        int num=183;
+        int pcs=0, total=0;
+
+
+        while(num!=0) {
+
+            total=(num%10)+total;
+            num/=10;
+            ++pcs;
+        }
+
+        System.out.println(total);
+
+    }
+
 }
